be95263dc58d3dfe442189b12ff3efe633fc0878
Jorge <jpadilla.developer@gmail.com>
2016-10-30 14:49:57 +0000

    Updated script clam scan
    Moves files between folders while analyzing, then moves it to a target folder if there are not virus found

--- shell/clamscan.sh
+++ shell/clamscan.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
+PATH=/home/user/watchedfolder/
+TARGET=/home/user/target/
 LOG_FILE=/home/user/log.txt
-FILE=/home/user/watchedfolder/$1
+ANALYZING_FILE=/home/user/analyzingfolder/$1
+FILE=$PATH$1
+OUTPUT=$TARGET$1
 COUNT=`/usr/bin/wc -l < $LOG_FILE`
-if [ "$COUNT" -gt "800" ]
+if [ "$COUNT" -gt "2000" ]
 then 
 	/bin/rm $LOG_FILE
 fi
-/usr/bin/clamscan --remove $FILE --log=$LOG_FILE
+
+/bin/mv $FILE $ANALYZING_FILE 
+/usr/bin/clamscan -r --remove $ANALYZING_FILE --log=$LOG_FILE
+
+if [ $? -eq 0 ]
+then
+	/bin/mv $ANALYZING_FILE $OUTPUT
+	/bin/echo "No Virus found in $1, moved to $OUTPUT" >> $LOG_FILE
+fi
+if [ $? -eq 1 ]
+then
+	/bin/echo "Virus found in $1, file removed by clam" >> $LOG_FILE
+fi
